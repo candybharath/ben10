30f77c58bb9c501e67f5d4873df6e2f7b514cc85
ashokkg06 <109447518+ashokkg06@users.noreply.github.com>
2023-01-20 14:19:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ashokkg06/Ben-filter-bot.git /Ben-filter-bot
+  git clone https://github.com/ashokkg06/Auto-filter-pro.git /Auto-filter-pro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ben-filter-bot
+  git clone $UPSTREAM_REPO /Auto-filter-pro
 fi
-cd /Ben-filter-bot
+cd /Auto-filter-pro
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
