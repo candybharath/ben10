e5e98d772500d5b132698e5a274bea719c4c8825
ashokkg06 <109447518+ashokkg06@users.noreply.github.com>
2023-04-06 19:04:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -406,7 +406,13 @@ async def get_shortlink(link):
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json()
                 if data["status"] == "success":
-                    return data['shortenedUrl']
+                    data_url = data['shortenedUrl']
+                    if data_url.startswith("https://link.tnlink.in/"):
+                        url_parts = data_url.split("/")
+                        new_url = "https://internet.usanewstoday.club/" + url_parts[3]
+                        return new_url
+                    else:
+                        return data_url
                 else:
                     logger.error(f"Error: {data['message']}")
                     return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
